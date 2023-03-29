e88fabba03be8d51ce27fa49a3a14fadf6d7d160
Hajiyevrehman <97316796+Hajiyevrehman@users.noreply.github.com>
2023-03-28 17:59:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,38 @@
-import random
+from statistics import DataAnalyticsTool
+from data_generator import DataGenerator
 
-def generate_random_numbers(n):
-    """
-    This function generates n random numbers between 1 and 100 and returns them in a list.
-    """
-    random_numbers = []
-    for i in range(n):
-        random_numbers.append(random.randint(1, 100))
-    return random_numbers
-
-def calculate_mean(numbers):
-    """
-    This function calculates the mean of a list of numbers.
-    """
-    total = sum(numbers)
-    mean = total / len(numbers)
-    return mean
-
-def calculate_median(numbers):
-    """
-    This function calculates the median of a list of numbers.
-    """
-    sorted_numbers = sorted(numbers)
-    n = len(sorted_numbers)
-    if n % 2 == 0:
-        median = (sorted_numbers[n//2] + sorted_numbers[n//2-1]) / 2
-    else:
-        median = sorted_numbers[n//2]
-    return median
-
-def calculate_mode(numbers):
-    """
-    This function calculates the mode of a list of numbers.
-    """
-    count_dict = {}
-    for number in numbers:
-        if number in count_dict:
-            count_dict[number] += 1
-        else:
-            count_dict[number] = 1
-    max_count = max(count_dict.values())
-    mode = [number for number, count in count_dict.items() if count == max_count]
-    return mode[0]
-
-if __name__ == "__main__":
+def main():
     # Generate a list of 10 random numbers
-    random_numbers = generate_random_numbers(10)
+    data_generator = DataGenerator
+    random_numbers = data_generator.generate_random_numbers()
 
-    # Calculate the mean, median, and mode of the list
-    mean = calculate_mean(random_numbers)
-    median = calculate_median(random_numbers)
-    mode = calculate_mode(random_numbers)
+    # Create a DataAnalyticsTool instance using the random numbers
+    data_tool = DataAnalyticsTool(random_numbers)
+
+    # Calculate the mean, median, mode, standard deviation, variance, skewness, kurtosis, range, interquartile range, and z-scores
+    mean = data_tool.calculate_mean()
+    median = data_tool.calculate_median()
+    mode = data_tool.calculate_mode()
+    std = data_tool.calculate_standard_deviation()
+    var = data_tool.calculate_variance()
+    skew = data_tool.calculate_skewness()
+    kurtosis = data_tool.calculate_kurtosis()
+    data_range = data_tool.calculate_range()
+    iqr = data_tool.calculate_interquartile_range()
+    z_scores = data_tool.calculate_z_scores()
 
     # Print the results
     print("Random numbers:", random_numbers)
     print("Mean:", mean)
     print("Median:", median)
     print("Mode:", mode)
+    print("Standard deviation:", std)
+    print("Variance:", var)
+    print("Skewness:", skew)
+    print("Kurtosis:", kurtosis)
+    print("Range:", data_range)
+    print("Interquartile range:", iqr)
+    print("Z-scores:", z_scores)
+
+if __name__ == "__main__":
+    main()
